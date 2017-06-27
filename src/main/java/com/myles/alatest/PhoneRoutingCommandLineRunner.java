4cd307e7a84ada8174695afbabc0855a1cd207e6
mylesieong <myles.bcm@gmail.com>
2017-06-27 10:41:02 +0800

    Rename the entrance class

---
+++ src/main/java/com/myles/alatest/PhoneRoutingCommandLineRunner.java
@@ -0,0 +1,11 @@
+package com.myles.alatest;
+
+/**
+ * 
+ *
+ */
+public class PhoneRoutingCommandLineRunner {
+    public static void main( String[] args ){
+        System.out.println( "Hello World!" );
+    }
+}
