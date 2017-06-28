91185771ed5022f9bbd38dfca896b4db42188a89
mylesieong <myles.bcm@gmail.com>
2017-06-28 11:20:38 +0800

    Impl stub search method in BruteSearch and delete depreciated PhoneOperatorTable class

--- src/main/java/com/myles/alatest/BruteSearch.java
+++ src/main/java/com/myles/alatest/BruteSearch.java
@@ -4,12 +4,28 @@ import java.util.Map;
 
 public class BruteSearch implements Search<String, Double>{
 
+    private Map<String, Double> map;
+
     @Override
     public Double search(String token){
-        return 1.5;
+        double result = -1;
+        int length = 0;
+
+        for ( Map.Entry<String, Double> t : this.map.entrySet() ){
+            String prefix = t.getKey();
+            int i = token.indexOf(prefix);
+            int l = prefix.length();
+            if ( i == 0 && l > length ){
+                result = t.getValue().doubleValue();
+                length = l; 
+            }
+        }
+
+        return new Double(result);
     } 
 
     @Override
     public void index(Map<String, Double> map){
+        this.map = map;
     }
 } 
